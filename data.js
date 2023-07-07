5aaf2348a6f873f590cca564952403fcf6b016ed
niteshkb <138812896+niteshkb@users.noreply.github.com>
2023-07-07 20:10:54 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://DJmaster:djmaster@cluster0.2an0b.mongodb.net/?retryWrites=true&w=majority', // mongo-link from cloud.mongodb.com
+    
+    bot_token:'6337553742:AAHCqKxDCNBKry122Z8dvwhlLjLipzuj6Fo',//bot token
+    
+    bot_name:'CeloAutopayingBot',//bot username without @
+    
+    bot_admin:5003571892,// admin telegram id
+    
+    channelsList : ['@LegalAirdrops','@AirdropUnicorns','@TheCryptoPumpCalls','@TechieEarning','TechieEarns'],// channels list
+
+    twitter:'https://twitter.com/BscGeminer?t=XA36W2gjOF31DGN0iGqXrA&s=09',//your twitter link
+    
+    reffer_bonus:0.004,// refferal bonus amount
+    
+    min_wd:0.020,//min withdrawal amount
+    
+    daily_bonus:0.001,// daily bonus amount
+    
+    currency:'CELO', //bot currency
+
+    contractaddress:'0x471ece3750da237f93b8e339c536989b8978a438',// contract address of coin
+
+    privatekey:'1b0fa35cbbc6efe7c82469630b3a0cd64607efea372a0618187a4ddac21ab7bd',// private key of wallet
+
+   address:' 0x94CBE822EC42E7fF56757f1F4AAc5e821c6Fe38a',// celo address of wallet
+    
+    payment_channel:'@RoyalEarner'// payment channel username
+    
+    }
